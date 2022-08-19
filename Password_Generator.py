b7a43b124e1d89ba634c2891140340aa060c1f06
ShlomoRoss <ishomr@gmail.com>
2022-08-19 15:41:24 -0400

    Added Password_Generator and updated README

--- Password_Generator.py
+++ Password_Generator.py
@@ -6,7 +6,6 @@ import random
 
 
 #Create dictionary of possible characters
-characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e',f'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*']
 letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e',f'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = ['1','2','3','4','5','6','7','8','9','0']
 symbols =['!','@','#','$','%','^','&','*']
@@ -20,7 +19,6 @@ pass_num = input("Would you like to include numbers in your password? [y/n] ")
 def which_pass():
 	if pass_sym.upper() == 'Y':
 		if pass_num.upper() == 'Y':
-			print("all")
 			all_pass()
 		else:
 			letters_sym_pass()
@@ -38,7 +36,7 @@ def which_pass():
 def all_pass():
 	password = ''
 	for n in range(0, int(len_pass)):
-		password += random.choice(characters)
+		password += random.choice(letters + numbers + symbols)
 	print(password)
 
 #if user wants letters and numbers, call this function
